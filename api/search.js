9ea6a46c7d371813e1c49f5c323e9ef6ced69b2a
Ezgt35 <bangezgt@gmail.com>
2024-08-22 16:04:53 +0700

    Update search.js

--- api/search.js
+++ api/search.js
@@ -3,9 +3,12 @@ const ytsr = require('ytsr');
 module.exports = async (req, res) => {
     const query = req.query.q;
     try {
+        console.log(`Searching for: ${query}`); // Log pencarian
         const searchResults = await ytsr(query, { limit: 10 });
+        console.log(`Results: ${JSON.stringify(searchResults.items)}`); // Log hasil pencarian
         res.status(200).json(searchResults.items);
     } catch (error) {
+        console.error(`Error: ${error.message}`); // Log error
         res.status(500).json({ error: 'Error fetching data from YouTube' });
     }
 };
